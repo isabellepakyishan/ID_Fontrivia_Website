66571b651b92c209f88b38de3d506facceeb9391
lanterex <s10204044@connect.np.edu.sg>
2022-02-04 18:22:53 +0800

    Copied template from Lottie CA to use here.

---
+++ loadingscript.js
@@ -0,0 +1,5 @@
+setTimeout(myFunction, 2000);
+
+function myFunction(){
+  window.location.href = "file:///D:/004%20IT%20YEAR%201%20SEM%202/ID/Week%2014/wk14-S10204044-EthanWong/simple-restDB/index.html";
+}
