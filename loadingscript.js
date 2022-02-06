0f8f448c3deeeb676359c2f27e01c246dc68d37b
lanterex <s10204044@connect.np.edu.sg>
2022-02-06 16:38:17 +0800

    Finished Loading Screen animation + random fact

--- loadingscript.js
+++ loadingscript.js
@@ -1,5 +1,17 @@
 setTimeout(myFunction, 2000);
 
 function myFunction(){
-  window.location.href = "file:///D:/004%20IT%20YEAR%201%20SEM%202/ID/Week%2014/wk14-S10204044-EthanWong/simple-restDB/index.html";
+  window.location.href = "#";
 }
+
+let url = 'https://uselessfacts.jsph.pl//random.json?language=en';
+
+let newprofile = function(){
+  fetch(url)
+  .then(response => response.json())
+  .then(function(data){
+    console.log(data)
+    $(".loading-text").html(data.text)
+  });
+}
+newprofile();
