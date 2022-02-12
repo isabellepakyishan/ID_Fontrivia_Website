f56a1e7057d74505815f50eb40fb7848d374faf4
lanterex <s10204044@connect.np.edu.sg>
2022-02-12 17:57:58 +0800

    Added video to LoadingPage BG (Haven't fixed)

--- loadingscript.js
+++ loadingscript.js
@@ -1,8 +1,7 @@
-setTimeout(loadHomePage, 4000);
+setTimeout(loadHomePage, 5000);
 
 function loadHomePage() {
-  window.location.href =
-    "file:///D:/ID/Assignment%202%20(Fontrivia%20Website)/ID_Fontrivia_Website/index.html";
+  window.location.href = "index.html";
 }
 
 let url = "https://uselessfacts.jsph.pl//random.json?language=en";
