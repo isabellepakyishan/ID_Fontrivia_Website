59cc6811f485e6ffdf26af08adfe589eac4464dd
isabelle_pak <s10222456@connect.np.edu.sg>
2022-02-12 15:26:32 +0800

    Added function to load page after loading screen

--- loadingscript.js
+++ loadingscript.js
@@ -1,17 +1,18 @@
-setTimeout(myFunction, 2000);
+setTimeout(loadHomePage, 4000);
 
-function myFunction(){
-  window.location.href = "#";
+function loadHomePage() {
+  window.location.href =
+    "file:///D:/ID/Assignment%202%20(Fontrivia%20Website)/ID_Fontrivia_Website/index.html";
 }
 
-let url = 'https://uselessfacts.jsph.pl//random.json?language=en';
+let url = "https://uselessfacts.jsph.pl//random.json?language=en";
 
-let newprofile = function(){
+let newprofile = function () {
   fetch(url)
-  .then(response => response.json())
-  .then(function(data){
-    console.log(data)
-    $(".loading-text").html(data.text)
-  });
-}
+    .then((response) => response.json())
+    .then(function (data) {
+      console.log(data);
+      $(".loading-text").html(data.text);
+    });
+};
 newprofile();
