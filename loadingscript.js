70c724b8852d8b65a559e1910086434aa9fe8141
lanterex <s10204044@connect.np.edu.sg>
2022-02-15 23:21:43 +0800

    used console.log to check fonts

--- loadingscript.js
+++ loadingscript.js
@@ -1,87 +1,90 @@
-const apikey = "6207718034fd6215658583f4";
+//ensures the document is working
+$(document).ready(function () {
+  const apikey = "6207718034fd6215658583f4";
 
-$("#registerForm").submit(() => false);
+  $("#registerForm").submit(() => false);
 
-function checkCurrentPage() {
-  //Script for loadingpage.html
-  if ($("body").hasClass("loadingpage")) {
-    setTimeout(loadLoginPage, 5000);
+  function checkCurrentPage() {
+    //Script for loadingpage.html
+    if ($("body").hasClass("loadingpage")) {
+      setTimeout(loadLoginPage, 5000);
 
-    function loadLoginPage() {
-      window.location.href = "login.html";
+      function loadLoginPage() {
+        window.location.href = "#";
+      }
+
+      let uselessFactsAPI =
+        "https://uselessfacts.jsph.pl//random.json?language=en";
+
+      let loadRandomFact = function () {
+        fetch(uselessFactsAPI)
+          .then((response) => response.json())
+          .then(function (data) {
+            console.log(data);
+            $(".loading-text").html(data.text);
+          });
+      };
+      loadRandomFact();
     }
 
-    let uselessFactsAPI =
-      "https://uselessfacts.jsph.pl//random.json?language=en";
-
-    let loadRandomFact = function () {
-      fetch(uselessFactsAPI)
-        .then((response) => response.json())
-        .then(function (data) {
-          console.log(data);
-          $(".loading-text").html(data.text);
-        });
-    };
-    loadRandomFact();
-  }
-
-  //Script for register.html
-  if ($("body").hasClass("register")) {
-    $("#psw, #psw-repeat").on("keyup", function () {
-      if ($("#psw").val() == $("#psw-repeat").val()) {
-        $("#message").html("Password is a match").css("color", "green");
-      } else {
-        $("#message").html("Password does not match").css("color", "red");
-      }
-    });
-
-    $("#register").on("click", function () {
-      alert("Starting registration");
-      let emailAdd = $("#email").val();
-      let password = $("#psw-repeat").val();
-      let jsondata = { "email-address": emailAdd, password: password };
-      let settings = {
-        async: true,
-        crossDomain: true,
-        url: "https://fontrivia-0c30.restdb.io/rest/user-login",
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-          "x-apikey": apikey,
-          "cache-control": "no-cache",
-        },
-        processData: false,
-        data: JSON.stringify(jsondata),
-      };
-
-      $.ajax(settings).done(function (response) {
-        console.log(response);
+    //Script for register.html
+    if ($("body").hasClass("register")) {
+      $("#psw, #psw-repeat").on("keyup", function () {
+        if ($("#psw").val() == $("#psw-repeat").val()) {
+          $("#message").html("Password is a match").css("color", "green");
+        } else {
+          $("#message").html("Password does not match").css("color", "red");
+        }
       });
-    });
-  }
 
-  //Script for login.html
-  if ($("body").hasClass("login")) {
-  }
+      $("#register").on("click", function () {
+        alert("Starting registration");
+        let emailAdd = $("#email").val();
+        let password = $("#psw-repeat").val();
+        let jsondata = { "email-address": emailAdd, password: password };
+        let settings = {
+          async: true,
+          crossDomain: true,
+          url: "https://fontrivia-0c30.restdb.io/rest/user-login",
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+            "x-apikey": apikey,
+            "cache-control": "no-cache",
+          },
+          processData: false,
+          data: JSON.stringify(jsondata),
+        };
 
-  //Script for index.html
-  if ($("body").hasClass("index")) {
-    let fontsAPI =
-      "https://www.googleapis.com/webfonts/v1/webfonts?sort=popularity&key=AIzaSyCYXVUXYdjLi5O-bJLmxgrpzQtiBv_T_ic";
-
-    let loadFont = function () {
-      fetch(fontsAPI)
-        .then((response) => response.json())
-        .then(function (data) {
-          console.log(data);
-          $(".loading-text").html(data.text);
+        $.ajax(settings).done(function (response) {
+          console.log(response);
         });
-    };
-    loadFont();
-  }
-}
+      });
+    }
 
-checkCurrentPage();
+    //Script for login.html
+    if ($("body").hasClass("login")) {
+    }
+
+    //Script for index.html
+    if ($("body").hasClass("index")) {
+      let fontsAPI =
+        "https://www.googleapis.com/webfonts/v1/webfonts?sort=popularity&key=AIzaSyCYXVUXYdjLi5O-bJLmxgrpzQtiBv_T_ic";
+
+      let loadFont = function () {
+        fetch(fontsAPI)
+          .then((response) => response.json())
+          .then(function (data) {
+            console.log(data);
+            $(".loading-text").html(data.text);
+          });
+      };
+      loadFont();
+    }
+  }
+
+  checkCurrentPage();
+});
 
 // // Script for Loading Page
 // setTimeout(loadLoginPage, 5000);
