f14ea24ec9fc0ec3a84515950b3b8220e5b2570e
isabelle_pak <s10222456@connect.np.edu.sg>
2022-02-17 18:13:18 +0800

    Modified script to show username of current user *Incomplete

--- JS/currentusername.js
+++ JS/currentusername.js
@@ -1,10 +1,34 @@
+// import {
+//     currentUsername
+// } from '../JS/login.js'
+
 (() => {
     const param = new URLSearchParams(window.location.search);
     const username = param.get('username');
     $("#current-username")[0].innerText = `Hello ${username}!`;
-    // if ($('body').hasClass('index')) {
+    if ($('body').hasClass('index')) {
+        const param = new URLSearchParams(window.location.search);
+        const username = param.get('username');
+        $("#current-username")[0].innerText = `Hello ${username}!`;
+    }
+
+    // if ($("body").hasClass("index")) {
     //     const param = new URLSearchParams(window.location.search);
     //     const username = param.get('username');
     //     $("#current-username")[0].innerText = `Hello ${username}!`;
+    //     window.location.href = `../../HTML/index.html?username=${currentUsername.finalUsername}`
+    // }
+    // if ($("body").hasClass("display")) {
+    //     const param = new URLSearchParams(window.location.search);
+    //     const username = param.get('username');
+    //     $("#current-username")[0].innerText = `Hello ${username}!`;
+    //     window.location.href = `../../HTML/Typography/display.html?username=${currentUsername.finalUsername}`
     // }
 })();
+
+// if ($("body").hasClass("display")) {
+//     const param = new URLSearchParams(window.location.search);
+//     const username = param.get('username');
+//     $("#current-username")[0].innerText = `Hello ${username}!`;
+//     window.location.href = `../../HTML/display.html?username=${currentUsername.finalUsername}`
+// }
