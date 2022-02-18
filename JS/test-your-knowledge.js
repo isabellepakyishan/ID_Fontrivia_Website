180049a1ca7e96bede263c4e97b930c2f75452f8
isabelle_pak <s10222456@connect.np.edu.sg>
2022-02-18 19:17:18 +0800

    Add script to store user updated data in nav bar

--- JS/test-your-knowledge.js
+++ JS/test-your-knowledge.js
@@ -88,3 +88,15 @@ gapi.load("client");
 $("#firstLoad").on("click", function () {
     $(this).hide();
 });
+
+(() => {
+    const username = sessionStorage.getItem('username')
+    const hasUsername = !(username === '' | username === null | username === undefined)
+
+    if (hasUsername) {
+        $("#current-username")[0].innerText = `Hello ${username}!`;
+        console.log(username)
+    }
+
+
+})();
